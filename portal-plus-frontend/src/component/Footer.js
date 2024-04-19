d9d27433979af184da48295214e7ae81bcffaaf4
ObadaSawalmeh <salimobada173@gmail.com>
2024-04-19 14:41:31 +0300

    edited the create course + generate qrCode pages

--- portal-plus-frontend/src/component/Footer.js
+++ portal-plus-frontend/src/component/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     <div className="bg-[#334e7d] text-white py-4 fixed bottom-0 w-full">
       <div className="container mx-auto">
         <p className="text-left">
-          &copy; Hashemite University.All rights reserved
+          &copy; Hashemite University. All rights reserved
         </p>
       </div>
     </div>
--- portal-plus-frontend/src/component/Header.js
+++ portal-plus-frontend/src/component/Header.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getName } from "../store";
 import { useEffect } from "react";
 
+// userPfp isn't showing
 function Header() {
   const dispatch = useDispatch();
   const { name } = useSelector((state) => {
@@ -13,7 +14,12 @@ function Header() {
   return (
     <div className="bg-[#334e7d] flex justify-between h-16 p-3">
       <p className="text-white mr-16 text-3xl">HU's PORTAL</p>
-      <p className="text-white text-lg">{name}</p>
+      <div className="flex justify-between items-center">
+        <div className="bg-white rounded-full h-9 w-9 flex justify-center items-center">
+          <img src='/userPfp.jpg' className="rounded-full"></img>
+        </div>
+        <p className="text-white text-lg px-4 font-medium"> {name}</p>
+      </div>
     </div>
   );
 }
